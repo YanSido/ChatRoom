cf09e96d3ce91263f307c174cf70f3428b57b473
YanSido <yansido1@gmail.com>
2021-12-01 20:09:02 +0200

    Added send message on enter press and clear message box

--- client/src/components/ChatScreen.js
+++ client/src/components/ChatScreen.js
@@ -12,14 +12,22 @@ function Chat(props) {
         })}
       </div>
       <textarea
-        onChange={(e) => {
-          setMessage(e.target.value);
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            setMessage(e.target.value);
+            setMessages((messages) => [...messages, message]);
+            setMessage("");
+          }
         }}
       ></textarea>
 
       <button
         onClick={(e) => {
           setMessages((messages) => [...messages, message]);
+          setMessage("");
         }}
       >
         Click
