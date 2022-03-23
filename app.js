e4578163deb7720d65cdddc28aa7316788202165
YanSido <yansido1@gmail.com>
2022-03-23 04:15:11 +0200

    make it better

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/public", express.static(`./client/public`));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.use("/", messagesRouter);
 
 module.exports = app;
